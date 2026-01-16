25671b04421de522338eeebbfd94286a280d100a
David Johnston <david@blacksheepcode.com>
2026-01-16 14:12:49 +1100

    Add class spy example.

--- src/foo.test.ts
+++
@@ -1,9 +0,0 @@
-import { vi, describe, it, expect } from "vitest";
-import * as foo from "./main";
-
-// This only errors if we are on browser mode
-vi.spyOn(foo, "foo");
-
-it("passes", () => {
-  expect(true).toBe(true);
-});
--- src/main.test.ts
+++
@@ -1,9 +0,0 @@
-import { vi } from "vitest";
-import * as lodash from "lodash";
-
-// This errors whether we are on browser mode or not.
-vi.spyOn(lodash, "after");
-
-it("passes", () => {
-  expect(true).toBe(true);
-});
